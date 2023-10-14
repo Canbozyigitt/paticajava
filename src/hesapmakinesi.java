80f951abdbd41074c9daea0fff03acfef1c4af65
CANO <umutcanbozyigit34012@gmail.com>
2023-10-14 22:24:32 +0300

    patica

---
+++ src/hesapmakinesi.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class hesapmakinesi {
+    public static void main(String[] args) {
+
+        Scanner input= new Scanner(System.in);
+        System.out.println("bir sayı giriniz ");
+        int sayi1=input.nextInt();
+        System.out.println(sayi1);
+
+
+    }
+}
